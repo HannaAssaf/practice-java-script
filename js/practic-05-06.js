e906f3a5fd90f84efc54b9d06e8d7a5876f6357c
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-02-16 15:41:34 +0000

    sdvsd

---
+++ js/practic-05-06.js
@@ -0,0 +1,18 @@
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+  }
+  
+  function goCave() {
+    console.log("Going to cave.");
+  }
+  
+  function fightDragon() {
+    console.log("Fighting dragon.");
+  }
+  
