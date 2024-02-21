10f2e1c39c10fb83e40d21068ff6f77a7d2dd01c
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-02-21 12:19:51 +0000

    спрсч

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -23,20 +23,4 @@
 
 
  
-const person = "John";
- 
-const person = "John";
-const context = {
-    person: "Alice"
-};
- 
-greet.call(context, "Bob");
-
-
-const numbers = [1, 2, 3];
-numbers[4] = 11;
-console.log(numbers);
-
-
-console.log(null >= 0); //true
 
