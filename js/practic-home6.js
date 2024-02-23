36bb7adaf33c20c0966a335b75a8b04cc47aeaeb
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-02-23 18:25:58 +0000

    lgm

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -20,24 +20,5 @@
 // console.log(null >= 0); //true
 
 
-____________________________________________________
-// Напишіть фукнцію, яка порівнює два масиви і повертає true, якщо вони ідентичні
-// Очікується([1, 2, 3], [1, 2, 3]) => true
-// Очікується([4, 5, 5], [1, 2, 3]) => false
-// Очікується([4, 5, 5,6,7], [1, 2, 3]) => false
-
-// const simpleArray = (firstArray, secondArray) => {
-//    if (firstArray.length !== secondArray.length) {
-//       return false;
-//    }
-//    else {
-//       for (let i=0; i<firstArray.length; i++) {
-//          if(firstArray[i] !== secondArray[i]) {
-//             return false;
-//          }
-//       }
-//    }
-//    return true;
-// }
 
 
