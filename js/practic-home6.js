06e64ed94ff91907d27ce0e4719f7f07d6b86e4d
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-03-05 21:46:46 +0000

    xfg

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -58,3 +58,24 @@ console.log(numbers);
 
 console.log(null >= 0); //true
 
+console.log(null >= 0); //true
+
+function greet(name) {
+    console.log(`Hello, ${name}! I am ${this.person}`);
+  }
+  
+  const person = "John";
+  const context = {
+    person: "Alice"
+  };
+  
+  greet.call(context, "Bob");
+  
+
+const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
+
+
+console.log(null >= 0); //true
+
