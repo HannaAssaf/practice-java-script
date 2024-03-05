ac571f3122d148cb3947f596134d5d038a562a43
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-03-05 21:46:18 +0000

    awefji

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -20,41 +20,41 @@
 // console.log(null >= 0); //true
 
 
-// function greet(name) {
-//     console.log(`Hello, ${name}! I am ${this.person}`);
-//   }
+function greet(name) {
+    console.log(`Hello, ${name}! I am ${this.person}`);
+  }
   
-//   const person = "John";
-//   const context = {
-//     person: "Alice"
-//   };
+  const person = "John";
+  const context = {
+    person: "Alice"
+  };
   
-//   greet.call(context, "Bob");
-  
-
-// const numbers = [1, 2, 3];
-// numbers[4] = 11;
-// console.log(numbers);
-
-
-// console.log(null >= 0); //true
-
-// function greet(name) {
-//     console.log(`Hello, ${name}! I am ${this.person}`);
-//   }
-  
-//   const person = "John";
-//   const context = {
-//     person: "Alice"
-//   };
-  
-//   greet.call(context, "Bob");
+  greet.call(context, "Bob");
   
 
-// const numbers = [1, 2, 3];
-// numbers[4] = 11;
-// console.log(numbers);
+const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
 
 
-// console.log(null >= 0); //true
+console.log(null >= 0); //true
+
+function greet(name) {
+    console.log(`Hello, ${name}! I am ${this.person}`);
+  }
+  
+  const person = "John";
+  const context = {
+    person: "Alice"
+  };
+  
+  greet.call(context, "Bob");
+  
+
+const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
+
+
+console.log(null >= 0); //true
 
