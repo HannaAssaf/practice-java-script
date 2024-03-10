0f833cea42494baea974ec906352f1e01eaad2e6
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-03-10 22:18:24 +0000

    vhj

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -78,4 +78,33 @@ console.log(numbers);
 
 
 console.log(null >= 0); //true
+  
+const person = "John";
+const context = {
+  person: "Alice"
+};
+
+greet.call(context, "Bob");
+
+
+const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
+
+
+console.log(null >= 0);  
+const person = "John";
+const context = {
+  person: "Alice"
+};
+
+greet.call(context, "Bob");
+
+
+const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
+
+
+console.log(null >= 0);
 
