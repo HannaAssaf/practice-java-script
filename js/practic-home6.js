eed4fb90181d0c967425bb7bdce9d8cfa56dbc11
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-03-13 18:30:45 +0000

    fsj

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -141,27 +141,5 @@ const context = {
   person: "Alice"
 };
 
-greet.call(context, "Bob");
 
-
-const numbers = [1, 2, 3];
-numbers[4] = 11;
-console.log(numbers);
-
-
-console.log(null >= 0);  
-const person = "John";
-const context = {
-  person: "Alice"
-};
-
-greet.call(context, "Bob");
-
-
-const numbers = [1, 2, 3];
-numbers[4] = 11;
-console.log(numbers);
-
-
-console.log(null >= 0);
 
