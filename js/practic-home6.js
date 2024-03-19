7fa2ef8efa247dc2612557ee106232ba217ef002
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2024-03-19 20:51:03 +0000

    njjhkbvh

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -308,7 +308,16 @@ console.log(null >= 0);
 
 const numbers = [1, 2, 3];
 numbers[4] = 11;
+const numbers = [1, 2, 3];const numbers = [1, 2, 3];
+numbers[4] = 11;
+console.log(numbers);
+
+
+console.log(null >= 0);
+
 const numbers = [1, 2, 3];
+numbers[4] = 11;
+
 numbers[4] = 11;
 console.log(numbers);
 
