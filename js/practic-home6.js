f691b434f80f22a45e3d6867792c4e5545612474
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-02 22:25:32 +0000

    03.02

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -352,6 +352,22 @@
 // console.log(x);
 // console.log(y);
 
-for (let i = 3; i <= 5; ++i) {
-  console.log(i);
+// for (let i = 3; i <= 5; ++i) {
+//   console.log(i);
+// }
+const sum;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    sum = quantity * pricePerDroid;
+    if (sum > customerCredits) {
+        return "Insufficient funds!";
+    }
+    else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+   }
 }
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
