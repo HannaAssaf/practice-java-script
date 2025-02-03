66a41c08f8f8660ce2f95b2ba193cef0b6affdf3
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-03 21:46:40 +0000

    xzv

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -358,15 +358,15 @@
 
 // "You ordered 10 droids worth 5000 credits!"
 
-function checkForSpam(message) {
-  let isMessege = toLowerCase(message);
-if ('spam' || 'sale') 
-}
+// function checkForSpam(message) {
+//   const lowerCaseMessage = message.toLowerCase();
+//   return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+// }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+// console.log(checkForSpam('Trust me, this is not a spam message')); // true
+// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
