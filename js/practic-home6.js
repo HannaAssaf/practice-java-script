4b201464e739813848dfe6f184a87b21d34bc84a
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-05 22:07:43 +0000

    Update practic-home6.js

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -352,21 +352,25 @@
 // console.log(x);
 // console.log(y);
 
-for (let i = 3; i <= 5; ++i) {
+// for (let i = 3; i <= 5; ++i) {
+//   console.log(i);
+// }
+
+// ('You ordered 10 droids worth 5000 credits!');
+
+// function checkForSpam(message) {
+//   const lowerCaseMessage = message.toLowerCase();
+//   return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+// }
+
+// console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+// console.log(checkForSpam('Trust me, this is not a spam message')); // true
+// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+for (let i = 0; 1 < 10; i++) {
   console.log(i);
 }
-
-('You ordered 10 droids worth 5000 credits!');
-
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
-}
-
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
