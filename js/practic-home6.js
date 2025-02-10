25b04e03dfb4fbfa892ad03f0cab190a84c88163
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-10 21:52:11 +0000

    cvjh

--- js/practic-home6.js
+++ js/practic-home6.js
@@ -677,3 +677,33 @@
 //     SUPERUSER: 'superuser',
 //   };
 // }
+
+// /**
+//  * Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю і
+//  * розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
+//  * кількістю з властивості stones.
+//  */
+
+// const chopShop = {
+//   stones: [
+//     { name: 'Emerald', price: 1300, quantity: 4 },
+//     { name: 'Diamond', price: 2700, quantity: 3 },
+//     { name: 'Sapphire', price: 1400, quantity: 7 },
+//     { name: 'Ruby', price: 800, quantity: 2 },
+//   ],
+//   calcTotalPrice(stoneName) {
+//     const findStone = this.stones.find(stone => {
+//       return stone.name === stoneName;
+//     });
+//     if (!findStone) {
+//       return 'Sorry. Your stone doesnt exist is store';
+//     }
+//     return findStone.price * findStone.quantity;
+//   },
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice("Diamond")); // 8100
+// console.log(chopShop.calcTotalPrice("Sapphire")); // 9800
+// console.log(chopShop.calcTotalPrice("Ruby")); // 1600
+// console.log(chopShop.calcTotalPrice('Sand'));
