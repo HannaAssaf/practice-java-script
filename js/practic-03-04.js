45b97fab496bda4a055fcd1970eb9b18aead94b6
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-12 21:57:37 +0000

    12.02

--- js/practic-03-04.js
+++ js/practic-03-04.js
@@ -1,9 +1,4 @@
-
 `use strict`;
-
-
-
-
 
 // todo Напиши скрипт, який для об’єкта user послідовно:
 //  - додасть поле mood зі значенням ‘happy’
@@ -29,20 +24,18 @@
 //    console.log(`${key} : ${user[key]}`);
 // }
 
-
-
 //    ___________________________________________________
-   // todo У нас є об‘єкт, у якому зберігаються заробітні плати команди.
-   // Напишіть код для отримання суми усіх зарплат і збережіть його результат у змінній sum
-   // Якщо об‘єкт salaries порожній, то результат повинен бути 0
-   
-   // const salaries = {
-   //  Mango: 100,
-   //  Poly: 160,
-   //  Ajax: 1470,
-   // }
+// todo У нас є об‘єкт, у якому зберігаються заробітні плати команди.
+// Напишіть код для отримання суми усіх зарплат і збережіть його результат у змінній sum
+// Якщо об‘єкт salaries порожній, то результат повинен бути 0
 
-   // let sum = 0;
+// const salaries = {
+//  Mango: 100,
+//  Poly: 160,
+//  Ajax: 1470,
+// }
+
+// let sum = 0;
 
 //   const arraySalaries = Object.values(salaries);
 //   for (const salary of arraySalaries) {
@@ -58,15 +51,15 @@
 //   console.log(sum);
 
 //    ___________________________________________________
-   // todo Напишіть функцію calcTotalPrice(stones, stonesName), яка приймає масив об‘єктів та рядок із назвою каменя.
-   // Функція рахує і повертає загальну вартість каменів з таким іменем, ціною та кількістю з об‘єкта
-  
-   // const stonesArray = [
-   //  { name: `Ізумруд`, price: 1300, quantity: 4 },
-   //  { name: `Брилліант`, price: 2700, quantity: 6 },
-   //  { name: `Сапфір`, price: 400, quantity: 7 },
-   //  { name: `Щебінь`, price: 150, quantity: 100 },
-   // ]
+// todo Напишіть функцію calcTotalPrice(stones, stonesName), яка приймає масив об‘єктів та рядок із назвою каменя.
+// Функція рахує і повертає загальну вартість каменів з таким іменем, ціною та кількістю з об‘єкта
+
+// const stonesArray = [
+//  { name: `Ізумруд`, price: 1300, quantity: 4 },
+//  { name: `Брилліант`, price: 2700, quantity: 6 },
+//  { name: `Сапфір`, price: 400, quantity: 7 },
+//  { name: `Щебінь`, price: 150, quantity: 100 },
+// ]
 // рішення:
 
 // function calcTotalPrice(stones, stonesName) {
@@ -79,11 +72,9 @@
 
 // console.log(calcTotalPrice(stonesArray, `Ізумруд`));
 
-
-
 //    ____________________________________________________
-   // todo Напишіть функцію updateObject, яка приймає об‘єкт і повертає новий об’єкт без вказаних параметрів
-   // Результат, що очікується ({a: 1, b: 2, c: 3}, 'b', 'a') => {c: 3}
+// todo Напишіть функцію updateObject, яка приймає об‘єкт і повертає новий об’єкт без вказаних параметрів
+// Результат, що очікується ({a: 1, b: 2, c: 3}, 'b', 'a') => {c: 3}
 
 // const updateObject = (obj, ...rest) => {
 //    const tempObj = {};
@@ -122,15 +113,15 @@
 //       return newObj;
 //    }, {});
 //   }
-  
+
 //   // Example usage:
 //   const obj = {a: 1, b: 2, c: 3};
 //   const updatedObj = updateObject(obj, 'b', 'a');
 //   console.log(updatedObj); // Output: {c: 3}
 
 //    ____________________________________________________
-   // Напишіть функцію, яка очистить масив від небажаних значень, таких як false, undefined, порожні рядки, нуль, null
-   // Результат, що очікується[0, 1, false, 2, undefined, ``, 3, null] => [1, 2, 3]
+// Напишіть функцію, яка очистить масив від небажаних значень, таких як false, undefined, порожні рядки, нуль, null
+// Результат, що очікується[0, 1, false, 2, undefined, ``, 3, null] => [1, 2, 3]
 
 //    function createNewArray(someArray) {
 // const newArray = [];
@@ -143,18 +134,18 @@
 //    }
 //    console.log(createNewArray([0, 1, false, 2, undefined, ``, 3, null]));;
 
-   // 2й спосіб:
+// 2й спосіб:
 
-   // function createNewArray(someArray) {
-   //    const newArray = [];
-   //    for (const item of someArray) {
-   //       if (item) {
-   //          newArray.push(item)
-   //       }
-   //    }
-   //    return newArray;
-   //       }
-   //       console.log(createNewArray([0, 1, false, 2, undefined, ``, 3, null]));
+// function createNewArray(someArray) {
+//    const newArray = [];
+//    for (const item of someArray) {
+//       if (item) {
+//          newArray.push(item)
+//       }
+//    }
+//    return newArray;
+//       }
+//       console.log(createNewArray([0, 1, false, 2, undefined, ``, 3, null]));
 
 // 3й спосіб:
 
@@ -162,10 +153,9 @@
 
 //       console.log(createNewArray);
 
-
 //    ____________________________________________________
 //    const arrNumber = [4, 5, 3, 1, 2]
-   // Напишіть функцію, яка розвертає масив у зворотньому напрямку.Метод reverse() не використовувати
+// Напишіть функцію, яка розвертає масив у зворотньому напрямку.Метод reverse() не використовувати
 
 //    const arrNumber = [4, 5, 3, 1, 2]
 //    function numbersArray(arr) {
@@ -175,8 +165,7 @@
 //       }
 // return newArray;
 //    }
-   // console.log(numbersArray(arrNumber));
-
+// console.log(numbersArray(arrNumber));
 
 // 2й спосіб:
 
@@ -185,7 +174,7 @@
 
 //    ____________________________________________________
 //    Напишіть функцію, яка повертає новий масив без вказаних значень.Використовуйте примітиви
-   // Очікується [1, 2, 3, 1, 2] без [1, 2] => [1,2,3]
+// Очікується [1, 2, 3, 1, 2] без [1, 2] => [1,2,3]
 
 // console.log([1, 2, 3, 1, 2].filter((currentValue, index, array) => array.indexOf(currentValue) === index));
 
@@ -193,13 +182,11 @@
 
 // console.log([1, 2, 3, 1, 2].filter((currentValue, index, array) => console.log(`indexOf: `, array.indexOf(currentValue), `index: `, index)));
 
-
-
 //    ____________________________________________________
-   // Напишіть фукнцію, яка порівнює два масиви і повертає true, якщо вони ідентичні
-   // Очікується([1, 2, 3], [1, 2, 3]) => true
-   // Очікується([4, 5, 5], [1, 2, 3]) => false
-   // Очікується([4, 5, 5,6,7], [1, 2, 3]) => false
+// Напишіть фукнцію, яка порівнює два масиви і повертає true, якщо вони ідентичні
+// Очікується([1, 2, 3], [1, 2, 3]) => true
+// Очікується([4, 5, 5], [1, 2, 3]) => false
+// Очікується([4, 5, 5,6,7], [1, 2, 3]) => false
 
 // const simpleArray = (firstArray, secondArray) => {
 //    if (firstArray.length !== secondArray.length) {
@@ -219,14 +206,120 @@
 // console.log(simpleArray([4, 5, 5], [1, 2, 3]));
 // console.log(simpleArray([4, 5, 5], [1, 2, 3]));
 
-
 //    ____________________________________________________
-   // Напишіть функцію, яка перетворює глибокий масив в одномірний. Не використовувати array.flat()
-   // Очікується[1, 2, [3, 4, [5]]] => [1, 2, 3, 4, 5]
+// Напишіть функцію, яка перетворює глибокий масив в одномірний. Не використовувати array.flat()
+// Очікується[1, 2, [3, 4, [5]]] => [1, 2, 3, 4, 5]
 
-   // const simpleArray = (array) => array.reduce((prev, currentValue) => prev.concat(Array.isArray(currentValue) ? simpleArray(currentValue) : currentValue), []);
+// const simpleArray = (array) => array.reduce((prev, currentValue) => prev.concat(Array.isArray(currentValue) ? simpleArray(currentValue) : currentValue), []);
 
-   // console.log(simpleArray([1, 2, [3, 4, [5]]]));
+// console.log(simpleArray([1, 2, [3, 4, [5]]]));
 
- 
+//Напиши функцію findSmallerNumber(numbers)
+//яка шукає найменше число в масиві.
+//Додай перевірку що функція отримує масив
 
+const numbers = [12, 5, 35, 56, 3, 24, 7, 80, 12];
+
+function findSmallerNumber(numbers) {
+  if (Array.isArray(numbers)) {
+    return Math.min(...numbers);
+  }
+}
+
+console.log(findSmallerNumber(numbers));
+
+//Напиши функцію findLongestWord(string)
+// //яка приймає довільний рядок
+// //що складається лише з розділених слів
+// //пробілом (параметр string)
+// //і повертає найдовше слово в цій строкі
+const string = 'Nunc sed turpis a felis in nunc fringilla';
+
+function findLongestWord(string) {
+  const arrayWords = string.split(' ');
+  let longestWord = arrayWords[0];
+  for (word of arrayWords) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+console.log(findLongestWord(string));
+
+// Напишіть функцію caclculateAverage()
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додати перевірку, що аргументи це числа.
+const arr = [2, 6, 24, 7, 35, 24, 'hello', 9, 'world'];
+
+function caclculateAverage(arr) {
+  const newArray = [];
+  let sum = 0;
+  for (let item of arr) {
+    if (typeof item === 'number') {
+      newArray.push(item);
+      sum += item;
+    }
+  }
+  return sum / newArray.length;
+}
+
+console.log(caclculateAverage(arr));
+
+//У нас є об'єкт, у якому зберігатимуться зарплати
+//нашої команди
+//Напишіть код для сумування всіх зарплат і
+//збережіть його результат у змінній sum.
+//Якщо об'єкт salaries порожній, то результат має бути 0
+
+const salaries = {
+  Mango: 300,
+  Ben: 100,
+  Poly: 160,
+  Ajax: 1470,
+  Bob: 450,
+};
+
+function sumSalaries(salaries) {
+  let sum = 0;
+  let arrSalaries = Object.values(salaries);
+  //   console.log(arrSalaries);
+
+  for (let item of arrSalaries) {
+    sum += item;
+  }
+  return sum;
+}
+sumSalaries(salaries);
+
+console.log(sumSalaries(salaries));
+console.log(sumSalaries([]));
+
+//  Напишіть функцію calcTotalPrice(fruits, fruitName),
+// яка приймає массив об'єктів (fruits) і рядок з назвою фрукта (fruitName).
+// Функція рахує і повертає загальну вартість фрукта
+// з таким ім'ям, ціною та кількістю з об'єкта.
+
+// Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою
+// назвою фрукта, це також треба урахувати.
+const fruits = [
+  { name: 'Яблуко', price: 45, quantity: 7 },
+  { name: 'Апельсин', price: 60, quantity: 4 },
+  { name: 'Банан', price: 125, quantity: 8 },
+  { name: 'Груша', price: 350, quantity: 2 },
+  { name: 'Виноград', price: 440, quantity: 3 },
+  { name: 'Банан', price: 125, quantity: 3 },
+];
+
+function calcTotalPrice(fruits, fruitName) {
+  let totalPrice = 0;
+  for (const item of fruits) {
+    if (item.name === fruitName) {
+      totalPrice += item.price * item.quantity;
+    }
+  }
+  return totalPrice;
+}
+
+console.log(calcTotalPrice(fruits, 'Банан'));
