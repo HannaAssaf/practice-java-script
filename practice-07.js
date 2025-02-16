42fb9d6429be7fd3155e38990ac83d94c5eafda5
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-02-16 23:09:26 +0000

    16.02-3

--- practice-07.js
+++ practice-07.js
@@ -27,13 +27,13 @@
  * https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480
  */
 
-const imageEl = document.querySelector('.hero__image');
-console.log('imageEl:', imageEl);
-const heroTitleEl = document.querySelector('.hero__title');
+// const imageEl = document.querySelector('.hero__image');
+// console.log('imageEl:', imageEl);
+// const heroTitleEl = document.querySelector('.hero__title');
 
 // Замінити підпис "About me"
 
-heroTitleEl.textContent = 'Hello from vs code';
+// heroTitleEl.textContent = 'Hello from vs code';
 
 // Змінити картинку на іншу:
 // 1й спосіб звернутися до атрибута:
@@ -41,17 +41,17 @@ const url =
   'https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480';
 // imageEl.setAttribute('src', url);
 // 2й спосіб звернутися як до властивості:
-imageEl.src = url;
+// imageEl.src = url;
 
-// Звернутися до атрибута:
-console.log(imageEl.alt);
-// або:
-console.log(imageEl.getAttribute('alt'));
+// // Звернутися до атрибута:
+// console.log(imageEl.alt);
+// // або:
+// console.log(imageEl.getAttribute('alt'));
 
-// Видалити атрибут:
-// console.log(imageEl.removeAttribute('src'));
+// // Видалити атрибут:
+// // console.log(imageEl.removeAttribute('src'));
 
-console.log(imageEl.hasAttribute('src'));
+// console.log(imageEl.hasAttribute('src'));
 
 // const actions = document.querySelectorAll('.actions button');
 
@@ -64,18 +64,30 @@ console.log(imageEl.hasAttribute('src'));
  * - contains
  */
 
-const currentPageUrl = '/contact';
+// const currentPageUrl = '/contact';
 // посилання на цю сторінку
-const linkEl = document.querySelector(
-  `.site-nav__link[href="${currentPageUrl}"]`
-);
+// const linkEl = document.querySelector(
+//   `.site-nav__link[href="${currentPageUrl}"]`
+// );
 // додати додатковий клас:
 
-linkEl.classList.add('styled-btn');
+// linkEl.classList.add('styled-btn');
 
 // linkEl.classList.remove('site-nav__link');
 
 // linkEl.classList.replace('site-nav__link', 'styled-btn');
 
 // чи є клас?
-console.log(linkEl.classList.contains('styled-btn'));
+// console.log(linkEl.classList.contains('styled-btn'));
+//
+//
+
+/**Створення та додавання елементів
+ * Створюємо заголовок
+ */
+
+const titleEl = document.createElement('h1');
+titleEl.textContent = 'Hello from JS';
+
+const hero22 = document.querySelector('div.hero22');
+hero22.append(titleEl);
