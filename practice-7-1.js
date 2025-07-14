7f98d8fda85471925c6017483aeb3065ccce9ebd
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-07-14 23:36:57 +0100

    js

--- practice-7-1.js
+++ practice-7-1.js
@@ -66,3 +66,30 @@ const {
 
 console.log(title); // "The Last Kingdom"
 console.log(bookCoverImage); // "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg"
+
+const book = {
+  title: 'The Last Kingdom',
+  coverImage:
+    'https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg',
+};
+
+const {
+  title,
+  coverImage: bookCoverImage = 'https://via.placeholder.com/640/480',
+} = book;
+
+console.log(title); // "The Last Kingdom"
+console.log(bookCoverImage); // "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg"
+const book = {
+  title: 'The Last Kingdom',
+  coverImage:
+    'https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg',
+};
+
+const {
+  title,
+  coverImage: bookCoverImage = 'https://via.placeholder.com/640/480',
+} = book;
+
+console.log(title); // "The Last Kingdom"
+console.log(bookCoverImage); // "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg"
